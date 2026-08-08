19d2edef672eaab27306c415dc65ff8715c90aba
phanuwat01 <Phanuwatwm2004@gmail.com>
2026-08-08 22:32:09 +0700

    แสดงปุ่มจบบันทึกเฉพาะหน้าสุดท้าย

--- script.js
+++ script.js
@@ -185,7 +185,10 @@ typing = false;
 nextBtn.style.display =
     currentPage === pages.length - 1 ? "none" : "inline-block";
 }
+const finishBtn = document.getElementById("finish-btn");
 
+finishBtn.style.display =
+    currentPage === pages.length - 1 ? "inline-block" : "none";
 document.getElementById("next-btn").addEventListener("click", function () {
 if (typing) return;
 if (currentPage < pages.length - 1) {
